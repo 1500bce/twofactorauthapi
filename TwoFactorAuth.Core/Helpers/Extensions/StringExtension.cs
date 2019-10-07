db5de9a03b94272ec5b745bd3d73c0113b6560ca
1500bce <36720373+1500bce@users.noreply.github.com>
2019-10-07 11:57:53 +0800

    changed content of extension ForceHttps()

--- TwoFactorAuth.Core/Helpers/Extensions/StringExtension.cs
+++ TwoFactorAuth.Core/Helpers/Extensions/StringExtension.cs
@@ -4,7 +4,11 @@
     {
         public static string ForceHttps(this string value)
         {
-            return value.Replace("http", "https");
+            UriBuilder uri = new UriBuilder(value);
+            uri.Scheme = Uri.UriSchemeHttps;
+            uri.Port = uri.Uri.IsDefaultPort ? uri.Port : -1;
+
+            return uri.ToString();
         }
 
         public static string ConstructMessage(this string message, string value)
